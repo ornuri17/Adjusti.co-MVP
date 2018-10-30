7896e0aa63843e701201db36f500383160aa03b8
OrNuri <ornuri17@gmail.com>
2018-10-30 17:56:18 +0200

    Change key path in exosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         production: {
             user: "ubuntu",
             host: "ec2-52-15-165-121.us-east-2.compute.amazonaws.com",
-            key: "C:/Users/Or/.ssh/adjustico.pem",
+            key: "~/.ssh/adjustico.pem",
             ref: "origin/master",
             repo: "git@github.com:ornuri17/Adjusti.co-MVP.git",
             path: "/home/ubuntu/adjustico",
